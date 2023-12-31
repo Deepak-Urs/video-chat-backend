25869769b6b88f3f6905b81f54cedd664802e9b5
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-12-31 14:34:41 -0800

    new backedn server repo

---
+++ index.js
@@ -0,0 +1,37 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+cors = require("cors")
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+app.use(cors())
+
+const PORT = process.env.port || 5000;
+
+app.get("/", (req, res) => {
+    res.send("New Server is running!")
+})
+
+io.on('connection', (socket) => {
+    // once socket connected we do
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended")
+    })
+
+    socket.on("calluser", ({ userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit("calluser", { signal: signalData, from, name})
+    })
+
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal)
+    })
+})
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
